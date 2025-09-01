e17c87a833fb651c3bdcd7fb94ef314f2232662e
falsofacu <facundotabarez0@gmail.com>
2025-09-01 15:53:08 -0300

    feat: implement pan and zoom functionality

--- public/game/main-game.js
+++ public/game/main-game.js
@@ -1,5 +1,6 @@
 import { isCooldownActive } from "./timer.js";
 import { resetTimer } from "./timer.js";
+import { updateTimer } from "./timer.js";
 import { getLastMouseCoords } from "./coordinates.js";
 
 const canvas = document.getElementById("canvas");
@@ -42,14 +43,9 @@ colorInput.addEventListener("change", (e) => {
 });
 
 // Paint a pixel on click
-canvas.addEventListener("click", (e) => {
-  const rect = canvas.getBoundingClientRect();
-  const scaleX = canvas.width / rect.width;
-  const scaleY = canvas.height / rect.height;
-
+/*canvas.addEventListener("click", (e) => {
   const [posX, posY] = getLastMouseCoords();
 
-  // Check if cooldown is active
   if (!isCooldownActive()) {
     resetTimer();
     // Paint pixel
@@ -59,6 +55,45 @@ canvas.addEventListener("click", (e) => {
     console.log("x: " + posX + " y: " + posY + " color: " + pickedColor);
   } else {
     alert("Wait for cooldown");
+  }
+}); */
+
+let startX, startY, moved;
+
+canvas.addEventListener("pointerdown", (e) => {
+  startX = e.clientX;
+  startY = e.clientY;
+  moved = false;
+});
+
+canvas.addEventListener("pointermove", (e) => {
+  if (startX == null) return;
+
+  const dragThreshold = 5; // Minimum distance in pixels to consider as a drag
+
+  if (
+    Math.abs(e.clientX - startX) > dragThreshold ||
+    Math.abs(e.clientY - startY) > dragThreshold
+  ) {
+    moved = true;
+  }
+});
+
+canvas.addEventListener("pointerup", (e) => {
+  if (!moved) {
+    const [posX, posY] = getLastMouseCoords();
+
+    if (!isCooldownActive()) {
+      updateTimer(); // Update timer display immediately
+      resetTimer();
+      // Paint pixel
+      updatePixelOnServer(posX, posY, pickedColor);
+      ctx.fillStyle = pickedColor;
+      ctx.fillRect(posX, posY, 1, 1);
+      console.log("x: " + posX + " y: " + posY + " color: " + pickedColor);
+    } else {
+      alert("Wait for cooldown");
+    }
   }
 });
 
@@ -91,3 +126,20 @@ function fetchCanvasFromServer() {
 const refreshCanvasInterval = setInterval(() => {
   fetchCanvasFromServer();
 }, 5000);
+
+//Pan and zoom
+const panzoom = Panzoom(canvas, {
+  maxScale: 16,
+  minScale: 2,
+  canvas: true,
+  overflow: "auto",
+});
+canvas.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  if (e.deltaY < 0) {
+    panzoom.zoomIn();
+  } else {
+    panzoom.zoomOut();
+  }
+});
+panzoom.setOptions({ cursor: "default" });
